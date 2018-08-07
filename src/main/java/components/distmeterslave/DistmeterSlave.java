0796eb7480bb3994044bf3859a705363f596218a
spitnix <35083721+spitnix@users.noreply.github.com>
2018-08-07 11:37:15 +0100

    Add files via upload
    with threads

--- src/main/java/components/distmeterslave/DistmeterSlave.java
+++ src/main/java/components/distmeterslave/DistmeterSlave.java
@@ -6,101 +6,123 @@ import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.ExecuteStreamHandler;
 import org.apache.commons.exec.PumpStreamHandler;
+import utilities.MessageSlave;
 
 
 import java.io.IOException;
 import java.io.File;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class DistmeterSlave  {
+public class DistmeterSlave implements Runnable {
 
     ConfManager confManager;
-    //ProcessorInfo cpuinfo;
-    //MemoryInfo meminfo;
     DismeterRESTClient rstCli;
-
+    private ConcurrentLinkedQueue<MessageSlave> myqueue;
+    private ConcurrentLinkedQueue<MessageSlave> myqueueCommands;
+    private ConcurrentLinkedQueue<MessageSlave> myqueueComServer;
     //Threads (
     //  to sync with Server
     //  to control tests (run commands)
+Thread tcommands;
+Thread tcomserver;
 
-
-    public DistmeterSlave(ConfManager confManager){
+    public DistmeterSlave(ConfManager confManager,ConcurrentLinkedQueue q, ConcurrentLinkedQueue q1, ConcurrentLinkedQueue q2, Thread tcommands) {
         this.confManager = confManager;
-        //this.cpuinfo = HardwareInfo.getProcessorInfo();
-        //this.meminfo = HardwareInfo.getMemoryInfo();
         this.rstCli = new DismeterRESTClient("localhost:8080");//TODO read from configuration
+        this.myqueue = q;
+        this.myqueueCommands = q1;
+        this.myqueueComServer = q2;
+        this.tcommands = tcommands;
     }
+
+
 
     /**
      * Method to register on the Distmeter server
      */
-    public boolean register(){
-        boolean res=false;
-        try{
+    public boolean register() {
+        boolean res = false;
+        try {
             Slaves slv = new Slaves();
-            slv.Name="test";
-            slv.SlaveID= (long)1;
+            slv.Name = "test";
+            slv.SlaveID = (long) 1;
             slv.n_cpus = 1;
             slv.mem_avai = 1;
             slv.mem_size = 1;
             res = rstCli.do_register(slv);
             return res; // success in register
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return res;
         }
     }
 
-    public void specs() {
-        /* Total number of processors or cores available to the JVM */
-        System.out.println("Available processors (cores): " +
-                Runtime.getRuntime().availableProcessors());
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()
+                + ", executing run() method!");
+        while(true) {
+            MessageSlave aux = myqueue.poll();
+            if (aux != null) {
+                System.out.println(aux);
+                switch (aux.getMsgtype()) {
+                    case MessageSlave.MESSAGETOCOMMANDS:
+                        myqueueCommands.add(aux); //CONTROLAR ESTADO DO PEDIDO
+                        System.out.println("ADICIONEI");
+                        break; //TODO RESTANTES TIPOS
+                }
 
-        /* Total amount of free memory available to the JVM */
-        System.out.println("Free memory (bytes): " +
-                Runtime.getRuntime().freeMemory());
-
-        /* This will return Long.MAX_VALUE if there is no preset limit */
-        long maxMemory = Runtime.getRuntime().maxMemory();
-        /* Maximum amount of memory the JVM will attempt to use */
-        System.out.println("Maximum memory (bytes): " +
-                (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory));
-
-        /* Total memory currently available to the JVM */
-        System.out.println("Total memory available to JVM (bytes): " +
-                Runtime.getRuntime().totalMemory());
-
-        /* Get a list of all filesystem roots on this system */
-        File[] roots = File.listRoots();
-
-        /* For each filesystem root, print some info */
-        for (File root : roots) {
-            System.out.println("File system root: " + root.getAbsolutePath());
-            System.out.println("Total space (bytes): " + root.getTotalSpace());
-            System.out.println("Free space (bytes): " + root.getFreeSpace());
-            System.out.println("Usable space (bytes): " + root.getUsableSpace());
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    public void execommand_op1(String cmd){
-        String line =cmd ;
-        CommandLine cmdLine = CommandLine.parse(line);
-        DefaultExecutor executor = new DefaultExecutor();
-        try {
-            int exitValue = executor.execute(cmdLine);
-            System.out.println("Exit value = " + exitValue);
-        }catch (IOException io){
-            io.printStackTrace();
-        }
+    public static class specs implements Runnable {
+        @Override
+        public void run() {
+            /* Total number of processors or cores available to the JVM */
+            System.out.println("Available processors (cores): " +
+                    Runtime.getRuntime().availableProcessors());
 
+            /* Total amount of free memory available to the JVM */
+            System.out.println("Free memory (bytes): " +
+                    Runtime.getRuntime().freeMemory());
+
+            /* This will return Long.MAX_VALUE if there is no preset limit */
+            long maxMemory = Runtime.getRuntime().maxMemory();
+            /* Maximum amount of memory the JVM will attempt to use */
+            System.out.println("Maximum memory (bytes): " +
+                    (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory));
+
+            /* Total memory currently available to the JVM */
+            System.out.println("Total memory available to JVM (bytes): " +
+                    Runtime.getRuntime().totalMemory());
+
+            /* Get a list of all filesystem roots on this system */
+            File[] roots = File.listRoots();
+
+            /* For each filesystem root, print some info */
+            for (File root : roots) {
+                System.out.println("File system root: " + root.getAbsolutePath());
+                System.out.println("Total space (bytes): " + root.getTotalSpace());
+                System.out.println("Free space (bytes): " + root.getFreeSpace());
+                System.out.println("Usable space (bytes): " + root.getUsableSpace());
+
+                System.out.println(Thread.currentThread().getName()
+                        + ", executing run() method!");
+            }
+        }
     }
 
     // Get output
     public void execommand_op2(String cmd){
         ExecuteStreamHandler streamHandler = new PumpStreamHandler(System.out, System.err, System.in);
-
         String line =cmd ;
         CommandLine cmdLine = CommandLine.parse(line);
-
         DefaultExecutor executor = new DefaultExecutor();
         executor.setStreamHandler(streamHandler);
         try {
@@ -113,5 +135,3 @@ public class DistmeterSlave  {
 
     }
 }
-
-
